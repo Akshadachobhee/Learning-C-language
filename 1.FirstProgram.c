bf55e1b503d291b7c899bf5668f5ae2ac6cf3530
Akshadachobhee <102922884+Akshadachobhee@users.noreply.github.com>
2023-03-16 23:32:57 +0530

    Add files via upload

---
+++ 1.FirstProgram.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a=11;
+    int b=-11;
+    signed int c=11;
+    unsigned int d=11;
+    printf("%d",&a);
+     printf("%d",&b);
+      printf("%d",&c);
+       printf("%d",&d);
+       return 0;
+}
